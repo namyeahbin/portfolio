52d09d7a61ccd003205f1a6fee3f36b91d19263c
namyeahbin <92068418+namyeahbin@users.noreply.github.com>
2022-03-14 14:15:39 +0900

    Update script.js

--- common/script/script.js
+++ common/script/script.js
@@ -38,6 +38,24 @@ $('.icon_btn').click(function() {
   $('.fullscreen').toggleClass('open');
  });
 
+// fadein 효과
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.hideme').each( function(i){
+          
+          var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_element ){
+              $(this).animate({'opacity':'1','margin-bottom':'500px'},1000);
+          }
+          
+      }); 
+  });
+});
+
+
+
 // skill 슬라이드 배너
 const swiper = new Swiper(".mySwiper", {
   pagination: {
